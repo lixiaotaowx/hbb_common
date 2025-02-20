e2d8b39291ec51f4c2c32bcfe1304929bac7994d
lixiaotao <103853019+lixiaotaowx@users.noreply.github.com>
2025-02-21 01:22:09 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["123.60.47.117"];
+pub const RS_PUB_KEY: &str = "4V0RT9vSHw+OhO7hv00JewtycOAQk8fTGsINxRj6tDY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
